4fed498cbc9a681d1b3c1d4637bc0de693009d19
YD <egordimeshko@gmail.com>
2026-09-14 13:44:00 +0300

    php add

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 <?php
-    echo "<h1>Hello World! Це моя перша PHP-програма.</h1>";
-    phpinfo();
+
+$appName = "Task Manager";
+$taskTitle = "Вивчити основи PHP";
+$taskTimeEstimate = 3;
+
 ?>
+
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <title><?= $appName ?></title>
+</head>
+<body>
+
+<header>
+    <h1><?= $appName ?></h1>
+</header>
+
+<main>
+    <ul>
+        <li><?= $taskTitle ?></li>
+        <li><?= $taskTimeEstimate ?> години</li>
+    </ul>
+</main>
+
+</body>
+</html>

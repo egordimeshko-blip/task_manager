d4078b3083b64cbdbf55b49ee62353b7c7040b60
Sipliy <egordimeshko@gmail.com>
2026-09-19 21:04:56 +0300

    jjk

--- index.php
+++ index.php
@@ -1,71 +1,66 @@
 <?php
-
 $appName = "Task Manager";
-
-$taskTitle = "Вивчити основи PHP";
+$taskTitle = "Вивчити основи PHP та створення власних функцій для форматування даних і локалізації дати";
 
 $taskTimeEstimate = 3;
-
 $isCompleted = true;
 
+function formatTitle($text, $maxLength = 20) {
+    if (strlen($text) > $maxLength) {
+        return substr($text, 0, $maxLength) . "...";
+    }
+    return $text;
+}
+
+function getCurrentGreeting() {
+    $hour = date('H');
+    if ($hour >= 6 && $hour < 12) {
+        return "Доброго ранку";
+    } elseif ($hour >= 12 && $hour < 18) {
+        return "Добрий день";
+    } elseif ($hour >= 18 && $hour < 24) {
+        return "Добрий вечір";
+    } else {
+        return "Доброї ночі";
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="uk">
-
 <head>
-
     <meta charset="UTF-8">
-
     <title><?= $appName ?></title>
-
     <style>
         .task-done {
             color: green;
             text-decoration: line-through;
         }
-
         .task-pending {
             color: gray;
         }
     </style>
-
 </head>
-
 <body>
 
 <header>
-
-    <h1><?= $appName ?></h1>
-
+    <h1><?= getCurrentGreeting() ?></h1>
 </header>
 
 <main>
-
     <ul>
-
         <li class="<?= $isCompleted ? 'task-done' : 'task-pending' ?>">
-
-            <?= $taskTitle ?>
+            <?= formatTitle($taskTitle) ?>
 
             <?php if ($isCompleted): ?>
-
                 Виконано
-
             <?php else: ?>
-
                 В процесі
-
             <?php endif; ?>
-
         </li>
-
         <li><?= $taskTimeEstimate ?> години</li>
-
     </ul>
-
 </main>
 
 </body>
-
 </html>
---
+++ lab4.md
@@ -0,0 +1,29 @@
+# Лабораторна робота №4
+**Тема:** Форматування даних власними функціями  
+**Мета:** Навчитися створювати власні функції для обробки тексту та локалізації дати, зрозуміти принципи DRY та область видимості змінних.  
+**Технології:** PHP, функції роботи з рядками, синтаксис `function`
+
+## Код програми
+
+```php
+<?php
+function formatTitle($text, $maxLength = 20) {
+    if (strlen($text) > $maxLength) {
+        return substr($text, 0, $maxLength) . "...";
+    }
+    return $text;
+}
+function getCurrentGreeting() {
+    $hour = date('H');
+    if ($hour >= 6 && $hour < 12) {
+        return "Доброго ранку";
+    } elseif ($hour >= 12 && $hour < 18) {
+        return "Добрий день";
+    } elseif ($hour >= 18 && $hour < 24) {
+        return "Добрий вечір";
+    } else {
+        return "Доброї ночі";
+    }
+}
+$taskTitle = "Вивчити основи PHP та створення власних функцій для форматування даних";
+?>

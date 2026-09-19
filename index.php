3606184628089641baa262e9b560b141158c28de
Sipliy <egordimeshko@gmail.com>
2026-09-19 20:32:01 +0300

    kkk

--- index.php
+++ index.php
@@ -1,28 +1,71 @@
 <?php
 
 $appName = "Task Manager";
+
 $taskTitle = "Вивчити основи PHP";
+
 $taskTimeEstimate = 3;
 
+$isCompleted = false;
+
 ?>
+
 <!DOCTYPE html>
 <html lang="uk">
+
 <head>
+
     <meta charset="UTF-8">
+
     <title><?= $appName ?></title>
+
+    <style>
+        .task-done {
+            color: green;
+            text-decoration: line-through;
+        }
+
+        .task-pending {
+            color: gray;
+        }
+    </style>
+
 </head>
+
 <body>
 
 <header>
+
     <h1><?= $appName ?></h1>
+
 </header>
 
 <main>
+
     <ul>
-        <li><?= $taskTitle ?></li>
+
+        <li class="<?= $isCompleted ? 'task-done' : 'task-pending' ?>">
+
+            <?= $taskTitle ?>
+
+            <?php if ($isCompleted): ?>
+
+                Виконано
+
+            <?php else: ?>
+
+                В процесі
+
+            <?php endif; ?>
+
+        </li>
+
         <li><?= $taskTimeEstimate ?> години</li>
+
     </ul>
+
 </main>
 
 </body>
+
 </html>
